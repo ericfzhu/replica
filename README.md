339d058e12b8823516b03d79835cbff9508b6e3b
Eric Zhu <73148494+ericfzhu@users.noreply.github.com>
2023-10-20 00:40:18 +1100

    ResNeXt-50 implementation (?)

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# DSLR-ResNeXt
+# Primitive convolutional neural network for photo enhancement to DSLR qualities.
+
+Based off of [Aggregated Residual Transformations for Deep Neural Networks](https://arxiv.org/abs/1611.05431v2)
--- model.py
+++ model.py
@@ -1,120 +1,97 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
-# Initialize weights and biases
-def initialize_weights(module):
-    if isinstance(module, nn.Conv2d):
-        nn.init.normal_(module.weight.data, 0.0, 0.01)
-        if module.bias is not None:
-            nn.init.constant_(module.bias.data, 0.01)
+class ResNeXt(nn.Module):
+    def __init__(self) -> None:
+        super(ResNeXt, self).__init__()
+        self.inplanes = 64
 
-# Instance Normalization Layer
-class InstanceNorm(nn.Module):
-    def __init__(self, num_features):
-        super(InstanceNorm, self).__init__()
-        self.scale = nn.Parameter(torch.ones(num_features))
-        self.shift = nn.Parameter(torch.zeros(num_features))
-        self.eps = 1e-3
+        self.conv1      = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.bn1        = nn.BatchNorm2d(64)
+        self.relu       = nn.ReLU(inplace=True)
+        self.maxpool    = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1     = self._layer(planes=64, blocks=3)
+        self.layer2     = self._layer(planes=128, blocks=4, stride=2)
+        self.layer3     = self._layer(planes=256, blocks=6, stride=2)
+        self.layer4     = self._layer(planes=512, blocks=3, stride=2)
+        self.avgpool    = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc         = nn.Linear(512 * Block.expansion, 1000)
 
+        for module in self.modules():
+            if isinstance(module, nn.Conv2d):
+                nn.init.kaiming_normal_(module.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(module, nn.BatchNorm2d):
+                nn.init.constant_(module.weight, 1)
+                nn.init.constant_(module.bias, 0)
+
+
+    def _layer(self, planes, blocks, stride):
+        downsample = nn.Sequential(
+            nn.Conv2d(in_channels=self.inplanes, out_channels=planes * Block.expansion, kernel_size=1, stride=stride, bias=False),
+            nn.BatchNorm2d(planes * 4)
+        )
+        
+        layers = []
+        layers.append(Block(inplanes=self.inplanes, planes=planes, stride=stride, downsample=downsample))
+        self.inplanes = planes * 4
+
+        for _ in range(1, blocks):
+            layers.append(Block(inplanes=self.inplanes, planes=planes))
+        
+        return nn.Sequential(*layers)
+    
     def forward(self, x):
-        mean = x.mean([2, 3], keepdim=True)
-        var = x.var([2, 3], keepdim=True)
-        return self.scale * (x - mean) / torch.sqrt(var + self.eps) + self.shift
+        out = self.conv1(x) # 7x7, 64, stride=2
+        out = self.bn1(out)
+        out = self.relu(out)
+        out = self.maxpool(out) # 3x3, 64, stride=2
 
-# ResNet Generator Model
-class ResNetGenerator(nn.Module):
-    def __init__(self):
-        super(ResNetGenerator, self).__init__()
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=9, padding=4)
+        out = self.layer1(out) # 3x3, 256, stride=1
+        out = self.layer2(out) # 3x3, 512, stride=2
+        out = self.layer3(out) # 3x3, 1024, stride=2
+        out = self.layer4(out) # 3x3, 2048, stride=2
 
-        # Residual Block 1
-        self.conv2 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
-        self.in2 = InstanceNorm(64)
-        self.conv3 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
-        self.in3 = InstanceNorm(64)
-
-        # Residual Block 2
-        self.conv4 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
-        self.in4 = InstanceNorm(64)
-        self.conv5 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
-        self.in5 = InstanceNorm(64)
-
-        # Residual Block 3
-        self.conv6 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
-        self.in6 = InstanceNorm(64)
-        self.conv7 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
-        self.in7 = InstanceNorm(64)
-
-        # Residual Block 4
-        self.conv8 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
-        self.in8 = InstanceNorm(64)
-        self.conv9 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
-        self.in9 = InstanceNorm(64)
-
-        # Convolutional layers
-        self.conv10 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
-        self.conv11 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
-
-        # Final layers
-        self.conv12 = nn.Conv2d(64, 3, kernel_size=9, padding=4)
-
-    def forward(self, x):
-        c1 = F.relu(self.conv1(x))
-
-        # Residual 1
-        c2 = F.relu(self.in2(self.conv2(c1)))
-        c3 = F.relu(self.in3(self.conv3(c2))) + c1
-
-        # Residual 2
-        c4 = F.relu(self.in4(self.conv4(c3)))
-        c5 = F.relu(self.in5(self.conv5(c4))) + c3
-
-        # Residual 3
-        c6 = F.relu(self.in6(self.conv6(c5)))
-        c7 = F.relu(self.in7(self.conv7(c6))) + c5
-
-        # Residual 4
-        c8 = F.relu(self.in8(self.conv8(c7)))
-        c9 = F.relu(self.in9(self.conv9(c8))) + c7
-
-        # Convolutional
-        c10 = F.relu(self.conv10(c9))
-        c11 = F.relu(self.conv11(c10))
-
-        # Final
-        out = torch.tanh(self.conv12(c11)) * 0.58 + 0.5
+        out = self.avgpool(out) # 1x1, 2048
+        out = torch.flatten(out, 1)
+        out = self.fc(out)
 
         return out
+    
 
-# Adversarial Discriminator Model
-class Discriminator(nn.Module):
-    def __init__(self):
-        super(Discriminator, self).__init__()
-        self.conv1 = nn.Conv2d(3, 48, kernel_size=11, stride=4, padding=5)
-        self.conv2 = nn.Conv2d(48, 128, kernel_size=5, stride=2, padding=2)
-        self.conv3 = nn.Conv2d(128, 192, kernel_size=3, stride=1, padding=1)
-        self.conv4 = nn.Conv2d(192, 192, kernel_size=3, stride=1, padding=1)
-        self.conv5 = nn.Conv2d(192, 128, kernel_size=3, stride=2, padding=1)
-        self.fc1 = nn.Linear(128 * 7 * 7, 1024)
-        self.fc2 = nn.Linear(1024, 2)
+class Block(nn.Module):
+    def __init__(self, inplanes, planes, stride=1, downsample=None) -> None:
+        super(Block, self).__init__()
+        width_per_group = 4
+        conv_groups = 32
+        width = int(planes * (width_per_group / 64)) * conv_groups
+        self.conv1      = nn.Conv2d(inplanes, width, kernel_size=1, bias=False)
+        self.bn1        = nn.BatchNorm2d(width)
+        self.conv2      = nn.Conv2d(width, width, kernel_size=3, stride=stride, padding=1, groups=conv_groups, bias=False)
+        self.bn2        = nn.BatchNorm2d(width)
+        self.conv3      = nn.Conv2d(width, planes * 4, kernel_size=1, bias=False)
+        self.bn3        = nn.BatchNorm2d(planes * 4)
+        self.relu       = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        self.stride     = stride
 
     def forward(self, x):
-        x = F.leaky_relu(self.conv1(x), 0.2)
-        x = F.leaky_relu(self.conv2(x), 0.2)
-        x = F.leaky_relu(self.conv3(x), 0.2)
-        x = F.leaky_relu(self.conv4(x), 0.2)
-        x = F.leaky_relu(self.conv5(x), 0.2)
-        
-        x = x.view(x.size(0), -1)  # Flatten
+        identity = x
 
-        x = F.leaky_relu(self.fc1(x), 0.2)
-        x = F.softmax(self.fc2(x), dim=1)
-        return x
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
 
-# Initialize models and apply weight initializer
-generator = ResNetGenerator()
-generator.apply(initialize_weights)
+        out = self.conv2(out) # groups=32
+        out = self.bn2(out)
+        out = self.relu(out)
 
-discriminator = Discriminator()
-discriminator.apply(initialize_weights)
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.downsample:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
