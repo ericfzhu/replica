b39aade6974f69bbac61fb09918fbc8abade89f3
Eric Zhu <73148494+ericfzhu@users.noreply.github.com>
2024-01-11 15:50:52 +1100

    reload from previous checkpoint since loss blew up

--- dataloader.py
+++ dataloader.py
@@ -2,10 +2,11 @@ import os
 from pathlib import Path
 from typing import Union
 import torch
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset, DataLoader, Sampler
 from torchvision import datasets
 from PIL import Image
 import numpy as np
+import random
 
 # load data from data/iphone/training_data/iphone
 train_original_dir = Path('data/iphone/training_data/iphone')
@@ -17,16 +18,26 @@ test_dslr_dir = Path('data/iphone/test_data/patches/canon')
 
 IMAGE_SIZE = 100 * 100 * 3
 
+class RandomSampler(Sampler):
+    def __init__(self, data_source, train_size):
+        self.data_source = data_source
+        self.train_size = train_size
+
+    def __iter__(self):
+        return iter(random.sample(range(len(self.data_source)), self.train_size))
+
+    def __len__(self):
+        return self.train_size
+
 def get_dataloaders() -> Union[DataLoader, DataLoader]:
-    # train_indices = np.random.choice(np.arange(0, len(os.listdir(train_original_dir))), train_size, replace=False)
     train_indices = np.arange(0, len(os.listdir(train_original_dir)))
     test_indices = np.arange(0, len(os.listdir(test_original_dir)))
 
     train_dataset = CustomImageDataset(train_original_dir, train_dslr_dir, train_indices, IMAGE_SIZE)
     test_dataset = CustomImageDataset(test_original_dir, test_dslr_dir, test_indices, IMAGE_SIZE)
 
-    train_dataloader = DataLoader(train_dataset, batch_size=64, shuffle=True, num_workers=2)
-    test_dataloader = DataLoader(test_dataset, batch_size=64, shuffle=True, num_workers=2)
+    train_dataloader = DataLoader(train_dataset, batch_size=100, sampler=RandomSampler(train_dataset, 5000))
+    test_dataloader = DataLoader(test_dataset, batch_size=100, shuffle=True, num_workers=2)
 
     return train_dataloader, test_dataloader
 
