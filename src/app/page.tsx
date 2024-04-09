747f6a3722377d636a33bc74a877e8d1019de372
Eric Zhu <73148494+ericfzhu@users.noreply.github.com>
2024-04-09 17:04:11 +1000

    geistmono

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
 	return (
 		<main className="flex min-h-screen flex-col items-center gap-24 bg-white p-24">
 			<section className="flex flex-col items-center gap-12">
-				<h1 className="flex-wrap text-center text-4xl text-black">
+				<h1 className="max-w-4xl flex-wrap text-center text-4xl text-black">
 					Imitator: <span className="text-accent-500">Im</span>age-to-<span className="text-accent-500">I</span>mage{' '}
 					<span className="text-accent-500">T</span>ransl<span className="text-accent-500">at</span>i
 					<span className="text-accent-500">o</span>n with a <span className="text-accent-500">R</span>esNeXt-based GAN
@@ -14,12 +14,12 @@ export default function Home() {
 				<div className="flex gap-2">
 					<Link
 						href="https://github.com/ericfzhu/imitator"
-						className="bg-accent-500 border-accent-500 hover:bg-accent-400 rounded-full border-[1px] px-4 py-2 text-white duration-300">
+						className="rounded-full border-[1px] border-accent-500 bg-accent-500 px-4 py-2 text-white duration-300 hover:bg-accent-400">
 						Code
 					</Link>
 					<Link
 						href="https://huggingface.co/ericfzhu/imitator"
-						className="bg-accent-500 border-accent-500 hover:bg-accent-400 rounded-full border-[1px] px-4 py-2 text-white duration-300">
+						className="rounded-full border-[1px] border-accent-500 bg-accent-500 px-4 py-2 text-white duration-300 hover:bg-accent-400">
 						HuggingFace
 					</Link>
 				</div>
