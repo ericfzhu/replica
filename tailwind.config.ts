cc530263ae7f408cf12828d31d5c794500128349
Eric Zhu <73148494+ericfzhu@users.noreply.github.com>
2024-04-09 16:57:29 +1000

    init website

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,24 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
+	content: ['./src/pages/**/*.{js,ts,jsx,tsx,mdx}', './src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
+	theme: {
+		extend: {
+			colors: {
+				accent: {
+					100: '#E3F2FE',
+					200: '#C7E3FD',
+					300: '#AAD0FB',
+					400: '#93BEF7',
+					500: '#70A3F2',
+					600: '#517ED0',
+					700: '#385DAE',
+					800: '#23408C',
+					900: '#152B74',
+				},
+			},
+		},
+	},
+	plugins: [],
 };
 export default config;
