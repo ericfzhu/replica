d1e3dec6876632eed40c354237dfef1a7610f209
Eric Zhu <73148494+ericfzhu@users.noreply.github.com>
2024-07-27 06:27:52 +1000

    draft implementation

--- public/models/alexnet/model.py
+++ public/models/alexnet/model.py
@@ -1,8 +1,59 @@
-import torch
 import torch.nn as nn
 
 class AlexNet(nn.Module):
     def __init__(self) -> None:
         super(AlexNet, self).__init__()
+
+        # input of 256 x 256
+        # uses relus instead of softmaxes?
+
+        # they use something called brightness normalization
+        # sum runs over n adjacent kernel maps at the same spatial position
+
+        # overlapping pooling with s = 2 and z = 3
         
-        self.conv1 = 
+        self.conv1 = nn.Conv2d(3, 96, kernel_size=11, stride=4)
+        self.conv2 = nn.Conv2d(96, 256, kernel_size=5)
+        self.conv3 = nn.Conv2d(256, 384, kernel_size=3)
+        self.conv4 = nn.Conv2d(384, 384, kernel_size=3)
+        self.conv5 = nn.Conv2d(384, 384, kernel_size=3)
+
+        self.fc1 = nn.Linear(4096, 4096)
+        self.fc2 = nn.Linear(4096, 4096)
+        self.fc3 = nn.Linear(4096, 1000)
+
+        self.relu = nn.Relu()
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2)
+
+        self.dropout = nn.Dropout(0.5)
+        self.flatten = nn.Flatten()
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.conv2(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.conv3(x)
+        x = self.relu(x)
+
+        x = self.conv4(x)
+        x = self.relu(x)
+
+        x = self.conv5(x)
+        x = self.relu(x)
+
+        x = self.flatten(x)
+        x = self.dropout(x)
+        x = self.fc1(x)
+        x = self.relu(x)
+
+        x = self.dropout(x)
+        x = self.fc2(x)
+        x = self.relu(x)
+
+        x = self.fc3(x)
+        x = self.relu(x)
