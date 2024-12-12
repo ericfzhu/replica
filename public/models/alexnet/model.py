a2574d3ab5d5e81fd8cad931e63ac2754b6dec0d
Eric Zhu <73148494+ericfzhu@users.noreply.github.com>
2024-12-13 09:37:49 +1100

    fix model inner layer size

--- public/models/alexnet/model.py
+++ public/models/alexnet/model.py
@@ -11,18 +11,18 @@ class AlexNet(nn.Module):
             nn.LocalResponseNorm(size=5, alpha=0.0001, beta=0.75, k=2),
             nn.MaxPool2d(kernel_size=3, stride=2),
 
-            nn.Conv2d(96, 256, kernel_size=5),
+            nn.Conv2d(96, 256, kernel_size=5, padding=2),
             nn.ReLU(inplace=True),
             nn.LocalResponseNorm(size=5, alpha=0.0001, beta=0.75, k=2),
             nn.MaxPool2d(kernel_size=3, stride=2),
 
-            nn.Conv2d(256, 384, kernel_size=3),
+            nn.Conv2d(256, 384, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
 
-            nn.Conv2d(384, 384, kernel_size=3),
+            nn.Conv2d(384, 384, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
 
-            nn.Conv2d(384, 256, kernel_size=3),
+            nn.Conv2d(384, 256, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2),
         )
@@ -37,8 +37,30 @@ class AlexNet(nn.Module):
             nn.Linear(4096, num_classes),
         )
 
+        self._initialize_weights()
+
     def forward(self, x):
         x = self.features(x)
         x = torch.flatten(x, 1)
         x = self.classifier(x)
         return x
+    
+    
+    def _initialize_weights(self):
+        # Initialize all weights from N(0, 0.01)
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.normal_(m.weight, mean=0, std=0.01)
+                
+                # Set bias = 1 for conv2, conv4, conv5
+                # Set bias = 0 for conv1, conv3
+                if m.bias is not None:
+                    if m in (self.conv2[0], self.conv4[0], self.conv5[0]):
+                        nn.init.constant_(m.bias, 1)
+                    else:
+                        nn.init.constant_(m.bias, 0)
+            
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, mean=0, std=0.01)
+                # Initialize all FC layer biases to 1
+                nn.init.constant_(m.bias, 1)
